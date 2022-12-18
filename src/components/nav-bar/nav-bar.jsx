8ef5933a33a04b385d22833bf907b7ae086a8357
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2022-12-18 15:12:26 -0800

    3.9

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
 import { Container, Nav, Navbar, Button, Link } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
@@ -26,7 +24,7 @@ export function NavBar({ user }) {
 		<>
 				<Navbar className="nav mb-2" variant="dark" expand="lg">
 					<Container>
-						<Navbar.Brand href="/">MyFlix App!</Navbar.Brand>
+						<Navbar.Brand href="/">Date Night Movies!</Navbar.Brand>
 						<Navbar.Toggle aria-controls="basic-navbar-nav" />
         		<Navbar.Collapse>
 							<Nav className="align-items-start">
@@ -50,108 +48,4 @@ export function NavBar({ user }) {
 	);
 }
 
-=======
-import React from "react";
-import { Container, Nav, Navbar, Button, Link } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import "./nav-bar.scss";
-
-export function NavBar({ user }) {
-  const onLoggedOut = () => {
-		localStorage.clear();
-		window.open("/", "_self");
-	};
-
-  const isAuth = () => {
-		if (typeof window == "undefined") {
-			return false;
-		}
-		if (localStorage.getItem("token")) {
-				return localStorage.getItem("token");
-		} else {
-			return false;
-		}
-	};
-
-  return (
-		<>
-				<Navbar className="nav mb-2" variant="dark" expand="lg">
-					<Container>
-						<Navbar.Brand href="/">MyFlix App!</Navbar.Brand>
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-        		<Navbar.Collapse>
-							<Nav className="align-items-start">
-								<NavLink className="text m-2" to="/">
-									Home
-								</NavLink>
-								{isAuth() && (<NavLink className="text m-2" to={`/users/${user}`}>
-									Profile
-								</NavLink>)}
-								{!isAuth() && (<NavLink className="text m-2" to="/register">
-									Register
-								</NavLink>)}
-								{isAuth() && (<NavLink className="text m-2" onClick={onLoggedOut}>
-									Logout
-								</NavLink>)}
-							</Nav>
-						</Navbar.Collapse>
-					</Container>
-				</Navbar>
-		</>
-	);
-}
-
->>>>>>> 4a952d312500b67f164d060421a59facccc8e3c2
-=======
-import React from "react";
-import { Container, Nav, Navbar, Button, Link } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import "./nav-bar.scss";
-
-export function NavBar({ user }) {
-  const onLoggedOut = () => {
-		localStorage.clear();
-		window.open("/", "_self");
-	};
-
-  const isAuth = () => {
-		if (typeof window == "undefined") {
-			return false;
-		}
-		if (localStorage.getItem("token")) {
-				return localStorage.getItem("token");
-		} else {
-			return false;
-		}
-	};
-
-  return (
-		<>
-				<Navbar className="nav mb-2" variant="dark" expand="lg">
-					<Container>
-						<Navbar.Brand href="/">MyFlix App!</Navbar.Brand>
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-        		<Navbar.Collapse>
-							<Nav className="align-items-start">
-								<NavLink className="text m-2" to="/">
-									Home
-								</NavLink>
-								{isAuth() && (<NavLink className="text m-2" to={`/users/${user}`}>
-									Profile
-								</NavLink>)}
-								{!isAuth() && (<NavLink className="text m-2" to="/register">
-									Register
-								</NavLink>)}
-								{isAuth() && (<NavLink className="text m-2" onClick={onLoggedOut}>
-									Logout
-								</NavLink>)}
-							</Nav>
-						</Navbar.Collapse>
-					</Container>
-				</Navbar>
-		</>
-	);
-}
-
->>>>>>> 4a952d312500b67f164d060421a59facccc8e3c2
 export default NavBar;
