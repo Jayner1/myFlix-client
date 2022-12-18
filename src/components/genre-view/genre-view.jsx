baaa79283a56242199cb998108a7c8ca9d7054ae
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2022-12-18 14:57:22 -0800

    3.9

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,51 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Button, Card, Row, Col } from "react-bootstrap";
+import { useParams, Link } from "react-router-dom";
+import "./genre-view.scss";
 
 export function GenreView ({movies}) {
 
-export class GenreView extends React.Component {
-  keypressCallback(event) {
-    console.log(event.key);
-  }
+	const selectGenre = () => {
+		const { name } = useParams();
+		return movies.find((m) => m.Genre.Name === name);
+	}
 
-  componentDidMount() {
-    document.addEventListener('keypress', this.keypressCallback);
-  }
-  render() {
-    const { genre, onBackClick } = this.props;
-
-    return (
-      <Container>
-        <Card className='mt-5 bg-dark text-white' style={{ borderRadius: '15px' }}>
-          <Card.Body>
-            <Row className="mt-3 pl-3 pr-3">
-              <Col className="label">Genre:</Col>
-              <Col className="value">{genre.Name}</Col>
-            </Row>
-          </Card.Body>
-          <Card.Body>
-            <Row className="mt-3 pl-3 pr-3">
-              <Col className="label">Description:</Col>
-              <Col className="value">{genre.Description}</Col>
-            </Row>
-            <Button
-              className='mt-4'
-              onClick={() => {
-                onBackClick();
-              }}
-              variant="primary"
-            >
-              Back
-            </Button>
-          </Card.Body>
-        </Card>
-      </Container >
-    );
-  }
-}
-GenreView.propTypes = {
-  genre: PropTypes.shape({
-    Name: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-  }).isRequired,
-};
+	return (
+		<Row className="justify-content-center">
+			<Col md={8} className="mt-3">
+				<Card className="genre-view p-3">
+					<Card.Title>Genre</Card.Title>
+					<Card.Subtitle>{selectGenre().Genre.Name}</Card.Subtitle>
+					<Card.Text>{selectGenre().Genre.Description}</Card.Text>
+					<Link to={-1} className="mt-2">
+						<Button variant="outline-secondary">Back</Button>
+					</Link>
+				</Card>
+			</Col>
+		</Row>
+	);
+	}
